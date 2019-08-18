8fdd1286a2f0daed7c7c5cefdecda2826bdef036
zenebu <47273917+zenebu@users.noreply.github.com>
2019-08-18 14:26:11 +0100

    Change title name and project name (#2)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div>
       <header className="home-header">
-        <h1>
-          Graduation Project skeleton
-        </h1>
+        <h1>Final Alpha Project</h1>
       </header>
     </div>
   );

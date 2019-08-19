f4de285e08886d6a514cdac3da1a4aa2f54b1f55
nabraha <nebay.tedros@yahoo.com>
2019-08-19 22:17:52 +0100

    Modify MenuBar component to a class component

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,36 +1,39 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-const MenuBar = () => {
-  const [activeItem, setActiveItem] = useState("home");
+export default class MenuBar extends Component {
+  state = {
+    activeItem: "home"
+  };
 
-  const handleItemClick = (e, { name }) => setActiveItem(name);
-  return (
-    <Menu pointing secondary size="massive" color="brown">
-      <Menu.Item
-        name="home"
-        active={activeItem === "home"}
-        onClick={handleItemClick}
-        as={Link}
-        to="/"
-      />
-      <Menu.Item
-        name="about"
-        active={activeItem === "about"}
-        onClick={handleItemClick}
-        as={Link}
-        to="/about"
-      />
-      <Menu.Item
-        name="status"
-        active={activeItem === "status"}
-        onClick={handleItemClick}
-        as={Link}
-        to="/status"
-      />
-    </Menu>
-  );
-};
-
-export default MenuBar;
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <Menu pointing secondary size="massive" color="brown">
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={this.handleItemClick}
+          as={Link}
+          to="/"
+        />
+        <Menu.Item
+          name="about"
+          active={activeItem === "about"}
+          onClick={this.handleItemClick}
+          as={Link}
+          to="/about"
+        />
+        <Menu.Item
+          name="status"
+          active={activeItem === "status"}
+          onClick={this.handleItemClick}
+          as={Link}
+          to="/status"
+        />
+      </Menu>
+    );
+  }
+}

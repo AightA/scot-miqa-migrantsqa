1fd9ca337cd2a9331c02edf30e761192c3798cf3
nabraha <nebay.tedros@yahoo.com>
2019-08-19 19:27:53 +0100

    Remove comments from home page

--- client/src/index.js
+++ client/src/index.js
@@ -20,26 +20,3 @@ const Routes = () => {
 };
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
-
-{
-  /* <div>
-<ul>
-  <li>
-    <Link className="nav-link" to="/">
-      Home
-    </Link>
-  </li>
-  <li>
-    <Link className="nav-link" to="/about">
-      About
-    </Link>
-  </li>
-  <li>
-    <Link className="nav-link" to="/status">
-      Status
-    </Link>
-  </li>
-</ul>
-
-<div> */
-}

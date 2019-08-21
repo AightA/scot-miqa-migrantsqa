d15e66c051753e5690f5d8ead9118c624ef28c7b
reyam m <reyamm@reyams-MacBook-Air.local>
2019-08-21 21:32:02 +0100

    Design the questions DB table and add a SQL script that populates

--- server/db/recreate-schema.sql
+++ server/db/recreate-schema.sql
@@ -7,3 +7,23 @@ CREATE TABLE users (
   email    VARCHAR(200) NOT NULL,
   password VARCHAR(200) NOT NULL
 );
+
+insert into users (email,password) values('reyam@gmail.com','123455');
+insert into users (email,password) values('ahmed@gmail.com','73635');
+insert into users (email,password) values('zan@gmail.com','2516722');
+
+
+
+
+
+CREATE TABLE questions(
+  id           SERIAL PRIMARY KEY,
+  ques_text    VARCHAR (150) NOT NULL,
+  ques_date    DATE NOT NULL,
+  tags         VARCHAR(50),
+  isAnswered   BOOLEAN NOT NULL,
+  score        INT,
+  userid      INT REFERENCES users(id)
+);
+
+

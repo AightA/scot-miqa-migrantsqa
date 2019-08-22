775cb2c5e0f7ec2d13010db390f4ed1cf59ac3f0
saeed <saeeddadgar66@gmail.com>
2019-08-22 20:38:12 +0100

    Update about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container>
+      <div className="content">
+        <h2>Q & A</h2>
+        <div className="description">
+          <p className="paragraph">
+            This forum is designed to help the newcomers to Glasgow with any
+            qustions they might have.<br></br>
+            Make the most of this fantastic webpage by signing up right now.
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default About;

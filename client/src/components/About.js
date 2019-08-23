fec459d26b38727fe507de1bf2ca0abc22e97761
saeed <saeeddadgar66@gmail.com>
2019-08-23 15:23:19 +0100

    comments put in practice

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,19 +4,19 @@ import { Container } from "semantic-ui-react";
 const About = () => {
   return (
     <Container>
-      <div className="content">
-        <h2>Q & A</h2>
-        <div className="description">
-          <p className="text-in-about">
-            This forum is designed to help the newcomers to Glasgow with any
-            questions they might have.<br></br>
-            Make the most of this fantastic webpage by signing up right now.
-            <br></br>
-            Feel free to ask any questions either in English or your mother
-            tongue.
-          </p>
-        </div>
-      </div>
+      <div class="ui huge header">Q & A</div>
+      <p className="text-in-about">
+        <p>
+          {" "}
+          This forum is designed to help the newcomers to Glasgow with any
+          questions they might have.
+        </p>
+        <p>Make the most of this fantastic webpage by signing up right now.</p>
+        <p>
+          Feel free to ask any questions either in English or your mother
+          tongue.
+        </p>
+      </p>
     </Container>
   );
 };

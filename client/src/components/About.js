bf9e0877715fb64c0fcb1402b049c72afc2c2fec
saeed <saeeddadgar66@gmail.com>
2019-08-23 12:40:01 +0100

    update the about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,10 +7,13 @@ const About = () => {
       <div className="content">
         <h2>Q & A</h2>
         <div className="description">
-          <p className="paragraph">
+          <p className="text-in-about">
             This forum is designed to help the newcomers to Glasgow with any
-            qustions they might have.<br></br>
+            questions they might have.<br></br>
             Make the most of this fantastic webpage by signing up right now.
+            <br></br>
+            Feel free to ask any questions either in English or your mother
+            tongue.
           </p>
         </div>
       </div>

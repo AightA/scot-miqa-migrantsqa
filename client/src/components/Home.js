312adcabcf9105416953bddade9b61e4781201d1
saeed <saeeddadgar66@gmail.com>
2019-09-07 12:10:49 +0100

    create space between the nav and search bar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "../styles/Home.css";
 import Search from "./HomePageSearch";
-import { Header, Container, Icon, Input } from "semantic-ui-react";
+import { Container, Divider, Icon, Input } from "semantic-ui-react";
 import Questions from "./QuestionsCard";
 const Home = () => {
   return (
     <Container>
       <header>
-        <Header as="h1">Graduation Project skeleton</Header>
-        <Search></Search>
+        <Divider horizontal />
+        <Search />
+        <Divider horizontal />
         <Input
           fluid
           icon={<Icon name="add" inverted circular link />}
           placeholder="Write your questions here"
         />
+        <Divider horizontal />
       </header>
       <Questions />
     </Container>

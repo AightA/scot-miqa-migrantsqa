6559cfc2fd371cfd07203a7f848ee39bd7deaf95
Irina Preda <irinapreda91@gmail.com>
2019-10-06 23:06:13 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const config = {
 	development: {
 		user: "app_user",
 		host: "localhost",
-		database: "miqa_final_project",
+		database: "final_project",
 		password: "password",
 		port: 5432,
 	},

e5450343e8e0e94d130d711cc05c97bbcff7e74c
youssef <youssef.sadek910@gmail.com>
2019-10-07 02:54:32 +0100

    Add sendmail to express route

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
 const users = require("./users");
 const questions = require("./questions");
 const answers = require("./answers");
-
-
 const status = require("./status");
+const sendMail = require("./sendMail");
 
 router.use("/users", users);
 router.use("/status", status);
 router.use("/questions", questions);
 router.use("/answers", answers);
+router.use("/answers", answers);
+router.use("/v1/communicate", sendMail);
+
 module.exports = router;

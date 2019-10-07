09229ab3155cddb88ec2c994c7492c772bbfc09b
youssef <youssef.sadek910@gmail.com>
2019-10-08 00:06:13 +0100

    Add the Domain Miqa

--- server/api/email-util.js
+++ server/api/email-util.js
@@ -1,7 +1,8 @@
 const mailgun = require("mailgun-js");
 exports.sendEmail = (recipient, message, attachment) =>
   new Promise((resolve, reject) => {
-    const DOMAIN = "sandbox31ad9b239544492b8277169c4c7b343c.mailgun.org";
+    // const DOMAIN = "sandbox31ad9b239544492b8277169c4c7b343c.mailgun.org";
+    const DOMAIN = "miqa.herokuapp.com";
     const mg = mailgun({
       apiKey: "8554c25a964940de4272e37f98dd5d5f-af6c0cec-5bb8acbf",
       domain: DOMAIN

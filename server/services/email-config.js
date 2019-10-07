69741f9865c8f31c05eba98f73c12498a652647a
youssef <youssef.sadek910@gmail.com>
2019-10-07 02:51:13 +0100

    Add mail Config

---
+++ server/services/email-config.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  const emailConfig = {
+    apiKey: "key8554c25a964940de4272e37f98dd5d5f-af6c0cec-5bb8acbf",
+    domain: "miqa.herokuapp.com"
+  };
+  return emailConfig;
+};
